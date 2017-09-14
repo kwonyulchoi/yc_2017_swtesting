ad2472faf4414b85c3c34eace1b1f70f3c0ff41b
hyojeong.park <btgg567@naver.com>
2017-09-14 20:29:40 +0900

    21360041 소형호

---
+++ Material/17-09-28_StructureBasedTesting/21360041/Practice/IsPrime/testframework/swut/swut_main.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include "libmodule.h"
+
+// Test template    
+
+class SampleTest : public testing::Test {
+  protected:
+    virtual void SetUp() {
+    }
+    virtual void TearDown() {
+    }
+};
+ 
+
+TEST_F(SampleTest, StatementCovTest) {
+	EXPECT_EQ(0, is_prime(1));
+	EXPECT_EQ(0, is_prime(4));
+	EXPECT_EQ(1, is_prime(2));
+	EXPECT_EQ(0, is_prime(15));
+}
+
+TEST_F(SampleTest, BranchDesicionCovTest) {
+
+}
+
+
+
+int main(int argc, char *argv[])
+{
+  char bus_addr[1024] ={ 0 , };
+  char cmd[2048] ={ 0 , };
+  int custom_argc=2;
+  char* pArgv[2];
+  char* pCmd1 =(char*)"--gtest_output=xml:swut_report.xml";
+  pArgv[1] = pCmd1;
+
+  ::testing::InitGoogleTest(&custom_argc,(char**)pArgv);
+  return RUN_ALL_TESTS();
+}
