3bff5362a222365522d276877f721208a1c43710
gktjgn33 <gktjgn33@naver.com>
2017-10-17 16:46:42 +0900

    superHA

--- Report/17-09-14_SpecBasedTesting/21360074/Practice/StateTransitionTest/testframework/swut/swut_main.cpp
+++ Report/17-09-14_SpecBasedTesting/21360074/Practice/StateTransitionTest/testframework/swut/swut_main.cpp
@@ -60,7 +60,36 @@ TEST_F(FSMTest, state_coverage_00) {
 }
 TEST_F(FSMTest, state_transition_coverage_00) {
   // Write Here
+  
+  // 현재 상태 확인 --> 잠김상태
+  EXPECT_EQ(&state_locked,m_pFsm->get_CurState());
+  
+  // 밀기 이벤트 발생 시키기
+  m_pFsm->trigger(EVT_PUSH,(void*)NULL); 
+  // locked 상태에서 밀기이벤트 발생 --> 그대로 locked 인지 검증
+  EXPECT_EQ(&state_locked,m_pFsm->get_CurState());
+  
+  
+  
+  
+  // 동전삽입 발생 시키기
+  m_pFsm->trigger(EVT_COIN,(void*)NULL); 
+  // 현재 상태 확인 --> 풀림상태
+  EXPECT_EQ(&state_unlocked,m_pFsm->get_CurState());
+
+  // unlocked에서 동전삽입  발생 시키기
+  m_pFsm->trigger(EVT_COIN,(void*)NULL); 
+  // 현재 상태 확인 --> 풀림상태
+  EXPECT_EQ(&state_unlocked,m_pFsm->get_CurState());
+
+  // 밀기 이벤트 발생 시키기
+  m_pFsm->trigger(EVT_PUSH,(void*)NULL); 
+  // 현재 상태 확인 --> 잠김상태
+  EXPECT_EQ(&state_locked,m_pFsm->get_CurState());
 }
+
+
+
 
 
 int main(int argc, char *argv[])
